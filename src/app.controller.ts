8d95c4b5440c10ec9aa388fe4b0d9aaf372be0a9
nouman1998 <nouman.ejaz198@gmail.com>
2020-08-15 13:42:25 +0500

    Delete and Update Controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,17 @@
 import { ProductDTO } from './DTO/ProductDto';
 import { Product } from './Model/Product';
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Put, Delete } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable, identity } from 'rxjs';
 
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-Product=[]
+
 
   @Get()
-  getHello(): string {
+  getHello(): Observable<any> {
     return this.appService.getHello();
   }
   @Get(":id")
@@ -20,7 +21,15 @@ Product=[]
   }
   @Post()
   addAnything(@Body() productDto :ProductDTO){
-    this.Product.push( this.appService.postProduct(productDto));
-    return this.Product;
+    // this.Product.push( this.appService.postProduct(productDto));
+    return  this.appService.postProduct(productDto);
   }
+  @Put(":id")
+    updateAnything(@Param("id") id, @Body() ProductDto:ProductDTO){
+    return this.appService.updateProduct(id,ProductDto);
+  }
+  @Delete(":id")
+  deleteProduct(@Param("id") id )
+  {
+    return this.appService.deleteProduct(id);}
 }
