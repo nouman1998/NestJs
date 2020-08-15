f7e7bf1a765acef9ea7968abb0b129b699f9e673
nouman1998 <nouman.ejaz198@gmail.com>
2020-08-15 13:39:58 +0500

    Put and Delete added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,31 @@
 import { Product } from './Model/Product';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Put } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { ProductDTO } from './DTO/ProductDto';
 
 @Injectable()
 export class AppService {
+  deleteProduct(id: any) {
+    let index= this.Product.findIndex((element)=>{return element.id == id });
+    this.Product.splice(index,1);
+    return this.Product;
+  }
+  Product=[]
+  updateProduct(id: any, ProductDto: ProductDTO) {
+   let index= this.Product.findIndex((element)=>{return element.id == id });
+   let product= new Product(id,ProductDto.name);
+   this.Product[index]= product;
+   return this.Product;
+  }
   postProduct(postDTO) {
     let product = new Product(new Date().getTime(),postDTO.name);
-    return product;
+    this.Product.push(product)
+    return this.Product;
   }
   getHelloById(id: any) {
     return `Hello World ${id}`;
   }
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): Observable<any> {
+    return of('Hello World!');
   }
 }
