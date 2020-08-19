5353742238794a937746e4fd3fb54912930a6b53
Nouman Ejaz <46034570+nouman1998@users.noreply.github.com>
2020-08-19 22:12:37 +0500

    Update CatsMiddleware.ts

--- src/cats/CatsMiddleware.ts
+++ src/cats/CatsMiddleware.ts
@@ -4,6 +4,6 @@ import { Request, Response } from 'express';
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function) {
     console.log('Request...');
-    // next();
+    next();
   }
 }
